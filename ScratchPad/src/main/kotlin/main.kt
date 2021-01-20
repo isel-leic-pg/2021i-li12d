c12d8b2cb4c8935457c1a760eb5a7399c86d0328
Paulo Pereira <palbp.giga@gmail.com>
2021-01-20 16:49:43 +0000

    Adds code written during the last session

--- ScratchPad/src/main/kotlin/main.kt
+++ ScratchPad/src/main/kotlin/main.kt
@@ -1,6 +1,48 @@
+import java.lang.IllegalArgumentException
 
+fun List<Int>.head(): Int? = if (this.isEmpty()) null else this[0]
+
+fun List<Int>.tail(): List<Int> = if (this.isEmpty())
+    emptyList()
+else this - this[0]
+
+fun min(v1: Int, v2: Int?): Int = when {
+    v2 == null -> v1
+    v1 < v2 -> v1
+    else -> v2
+}
+
+fun recursiveMin(list: List<Int>): Int? {
+    val head = list.head()
+    val tail = list.tail()
+    return if(head == null) null
+    else min(head, recursiveMin(tail))
+}
+
+
+fun someFun() {
+    val original: List<Int> = buildList(5, 10, 2).shuffled()
+    println("original -> $original")
+    val modified = original + 30
+    println("modified -> $modified")
+    println("original -> $original")
+
+    val mutableOriginal: MutableList<Int> = original.toMutableList()
+    println("mutableOriginal (first) -> $mutableOriginal")
+    mutableOriginal.add(100)
+    println("mutableOriginal (last) -> $mutableOriginal")
+}
 
 fun main() {
-    val arr = Array(5) { idx -> idx }
-    println(arr.toList())
+
+    val list = listOf(10, 20 , 30)
+    println(list)
+
+    val array = arrayOf(10, 20, 30)
+    val anotherArray = Array<Int>(1000) { idx -> idx }
+    array[0]
+    array[1]
+    array[2]
+    array[999]
+    println(array)
 }
--- ScratchPad/src/main/kotlin/sort.kt
+++ ScratchPad/src/main/kotlin/sort.kt
@@ -1,18 +1,44 @@
 
 fun min(v1: Int, v2: Int): Int = if (v1 < v2) v1 else v2
 
-fun List<Int>.min(): Int? {
-    if (this.isEmpty())
+fun List<Int>.indexOfMin(): Int? {
+
+    if (isEmpty())
         return null
 
-    var min = this[0]
-    var index = 1
+    var indexOfMin = 0
+    var min = this[indexOfMin]
 
+    var index = 1
     while (index < this.size) {
-        if (this[index] < min)
+        if (this[index] < min) {
             min = this[index]
+            indexOfMin = index
+        }
         index += 1
     }
 
-    return min
+    return indexOfMin
+}
+
+
+fun List<Int>.min(): Int? {
+    val index = this.indexOfMin()
+    return if(index != null) this[index]
+    else null
+}
+
+fun List<Int>.sort(): List<Int> {
+    var original = this
+    var result = emptyList<Int>()
+
+    while(result.size != this.size) {
+        val min = original.min()
+        if (min != null) {
+            original = original - min
+            result = result + min
+        }
+    }
+
+    return result
 }
